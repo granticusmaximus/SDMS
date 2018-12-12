01fe202f1f84407768845dca7183020026f0651e
Grant Watson <lordcappello@outlook.com>
2018-12-11 22:47:49 -0500

    Commit

---
+++ SDMS.App/DataAccess/GritDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SDMS.App.DataAccess
+{
+    public class GritDbContext
+    {
+    }
+}
---
+++ SDMS.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDMS.App.Startup))]
+namespace SDMS.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
